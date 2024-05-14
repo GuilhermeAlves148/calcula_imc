a6dc83f784495068e5b7be80eab194547725ba8a
Guilherme <alvesguilherme148117@gmail.com>
2024-05-13 22:31:36 -0300

    corrigindo e aperfeiçoando oprojeto

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,12 @@ import { useState } from 'react'
 import Calculadora from './components/Calculadora';
 
 function App() {
-  const [peso, setPeso] = useState('0');
-  const [altura, setAltura] = useState('0');
 
   return (
-    <div className='container'>
-      <form>
-        <h1 className='title'>Calculadora de IMC</h1>
-        <ul>
-          <li className='topic'>
-            <p className='placeholder'>Insira seu peso (KGs) </p>
-            <input type="number" onBlur={(e) => setPeso(e.target.value)} />
-          </li>
-          <li className='topic'>
-            <p className='placeholder'>Insira sua altura (Metros) </p>
-            <input type="number" onBlur={(e) => setAltura(e.target.value)}/>
-          </li>
-        </ul>
-        <>
-          <Calculadora peso={peso} altura={altura} />
-        </>
-      </form>
-    </div>
+    <>
+      <Calculadora />
+    </>
   )
-}
+  }
 
 export default App
--- src/components/Calculadora/index.jsx
+++ src/components/Calculadora/index.jsx
@@ -1,35 +1,70 @@
-import './main.css'
+import { useState } from "react"
 
-const Calculadora = ({ peso, altura }) => {
-    const calculaImc = peso / altura
-    const result = calculaImc / altura
+const Calculadora = () => {
+    const [peso, setPeso] = useState(0);
+    const [altura, setAltura] = useState(0);
 
-    console.log(result);
+    const calculaImc = () => {
+        const calculo1 = altura * altura;
+        const result = peso / calculo1;
 
-    if (result <= 18.5 ) {
-        return (
-            <p className='resultado'>{parseFloat(result).toFixed(2)}. Você está abaixo do peso.</p>
-        )
-    } else if (result >= 18.6 && result < 24.10) {
-        return (
-            <p className='resultado'>{parseFloat(result).toFixed(2)}. Você está no peso ideal. Parabéns.</p>
-        )
-    } else if (result >= 25.0 && result < 29.10 ) {
-        return (
-            <p className='resultado'>{parseFloat(result).toFixed(2)}. Você está levemente acima do peso.</p>
-        )
-    }  else if (result >= 30.0 && result < 34.10 ) {
-        return (
-            <p className='resultado'>{parseFloat(result).toFixed(2)}. Você está com obesidade grau I.</p>
-        )
-    }  else if (result >= 35.0 && result < 39.10 ) {
-        return (
-            <p className='resultado'>{parseFloat(result).toFixed(2)}. Você está com obesidade grau II. (Obesidade severa.)</p>
-        )
-    }  else if (result >= 40) {
-        return (
-            <p className='resultado'>{parseFloat(result).toFixed(2)}. Você está com obesidade grau III. (Obesidade mórbida.)</p>
-        )
+    if(result >= 18.5 && result <= 24.9){
+        return(
+            <>
+            <p><b>IMC: {result.toFixed(2)}</b></p>
+            <p>Você está no peso ideal. Parabéns.</p>
+            </>
+            )
+        } else if(result < 18.5){
+        return(
+            <>
+            <p><b>IMC: {result.toFixed(2)}</b></p>
+            <p> Você está abaixo do peso.</p>
+            </>
+            )
+        } else if(result >= 25 && result <= 29.9){
+        return(
+            <>
+            <p><b>IMC: {result.toFixed(2)}</b></p>
+            <p>Você está levemente acima do peso.</p>
+            </>
+            )
+        } else if(result >= 30 && result <= 34.9){
+        return(
+            <>
+            <p><b>IMC: {result.toFixed(2)}</b></p>
+            <p>Você está com obesidade grau I.</p>
+            </>
+            )
+        } else if(result >= 35 && result <= 39.9){
+        return(
+            <>
+            <p><b>Seu IMC: {result.toFixed(2)}</b></p>
+            <p>Você está com obesidade grau II. (Obesidade severa.)</p>
+            </>
+            )
+        } else if(result >= 40){
+        return(
+            <>
+            <p><b>Seu IMC: {result.toFixed(2)}</b></p>
+            <p> Você está com obesidade grau III. (Obesidade mórbida.)</p>
+            </>
+            )
+        } 
     }
+
+    return(
+        <div className="container">
+            <div className="conteudo">
+                <h1 className="title">Calculadora de IMC</h1>
+                <p className="placeholder">Digite sua altura (Metros):</p>
+                <input type="number" onBlur={e => setAltura(e.target.value)} placeholder="Exemplo: 1.70" />
+                <p className="placeholder">Digite seu peso (KGs):</p>
+                <input type="number" onBlur={e => setPeso(e.target.value)} placeholder="Exemplo: 69.2" /> <br/>
+                {peso !== 0 && altura !== 0 && (calculaImc())}
+            </div>
+        </div>
+    )
+
 }
 export default Calculadora;
